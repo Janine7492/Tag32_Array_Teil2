a44346aa04eeec03728c32c41933bd728409799a
janine7492 <janine.supercode@gmail.com>
2022-11-03 14:36:13 +0100

    first commit

---
+++ Level1_1_js-vertief._array_sort/assets/js/main.js
@@ -0,0 +1,16 @@
+let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+];
+
+const sortierung = console.log(languages.sort());
---
+++ Level1_3_js-vertief._array-sort_number-sort/assets/js/main.js
@@ -0,0 +1,3 @@
+const numArray1 = [36, 24, 22, 3, 2, 98, 88, 99, 10, 54, 68, 70];
+
+console.log(numArray1.sort((a, b) => { return a - b; }));
